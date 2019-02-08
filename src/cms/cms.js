4d451a03288d3982a3b9846857677373b500b7bd
dbertella <daniele.bertella@gmail.com>
2019-02-08 01:05:49 +0000

    Add menu preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,10 @@ import CMS from 'netlify-cms'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('thanks', AboutPagePreview)
 CMS.registerPreviewTemplate('news', BlogPostPreview)
+CMS.registerPreviewTemplate('menu', ProductPagePreview)
