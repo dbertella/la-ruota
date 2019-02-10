7cc6127b5dda268c22baef7d0b7de451b89f0d94
dbertella <daniele.bertella@gmail.com>
2019-02-10 11:36:37 +0000

    Fix home page preview

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -2,14 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { HomePageTemplate } from '../../templates/home-page'
 
-const HomePagePreview = ({ entry, widgetFor }) => (
-  <HomePageTemplate
-    title={entry.getIn(['data', 'title'])}
-    subtitle={entry.getIn(['data', 'subtitle'])}
-    carousel={entry.getIn(['data', 'carousel'])}
-    content={widgetFor('body')}
-  />
-)
+const HomePagePreview = ({ entry, widgetFor }) => {
+  const carouselSlides = entry.getIn(['data', 'carousel'])
+  const slides = carouselSlides ? carouselSlides.toJS() : []
+  return (
+    <HomePageTemplate
+      title={entry.getIn(['data', 'title'])}
+      subtitle={entry.getIn(['data', 'subtitle'])}
+      carousel={{ slides }}
+      content={widgetFor('body')}
+    />
+  )
+}
 
 HomePagePreview.propTypes = {
   entry: PropTypes.shape({
